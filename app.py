ee7b814cb5893fc6994301836148d26c632f1e9d
Omer_Bawazir <131506919+Omer-Secure@users.noreply.github.com>
2025-01-22 02:45:41 -0800

    Update app.py

--- app.py
+++ app.py
@@ -627,7 +627,9 @@ def ransomware_malware():
 
 @app.route('/')
 def homepage():
-    return redirect('https://omer-secure.github.io')
+    return redirect('https://omer-secure.onrender.com/homoglyph-spoofing-detector')
+
+''' --- return redirect('https://omer-secure.github.io') --- '''
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
